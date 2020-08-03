aff3231b702abb7f25121a52fa3e5ff9d368b5c8
Andreazo Silva <andreazocoisas@gmail.com>
2020-08-03 15:32:04 -0300

    first commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,135 @@
+import React, { useRef, useCallback } from 'react';
+import { Image,Alert, View, TextInput, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErros';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import Icon from 'react-native-vector-icons/Feather';
+import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+
+import {
+    Container,
+    Title,
+    BackToSignIn,
+    BackToSignInText
+
+} from './styles';
+
+
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+    const emailRef = useRef<TextInput>(null);
+    const passwordRef = useRef<TextInput>(null);
+    const navigation = useNavigation();
+
+
+    const handleSignUp = useCallback(async (data: SignUpFormData) => {
+        try {
+            formRef.current?.setErrors({});
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string()
+                    .min(6, 'No mínimo 6 dígitos')
+            })
+
+            await schema.validate(data, {
+                abortEarly: false
+            });
+
+
+        } catch (error) {
+            if (error instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(error);
+                formRef.current?.setErrors(errors);
+                return;
+            }
+
+            Alert.alert('Erro no cadastro',
+                'Ocorreu um erro ao fazer o cadastro, tente novamente.');
+        }
+
+    }, [])
+
+
+    return (
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    contentContainerStyle={{ flex: 1 }}
+                >
+                    <Container>
+                        <Image source={logoImg} />
+                        <View>
+                            <Title>Crie sua conta</Title>
+                        </View>
+                        <Form
+                            ref={formRef}
+                            onSubmit={handleSignUp}
+                        >
+                            <Input
+
+                                autoCapitalize="words"
+                                name="name"
+                                icon="user"
+                                placeholder="Nome"
+                                returnKeyType="next"
+                                onSubmitEditing={() => emailRef.current?.focus()}
+                            />
+
+                            <Input
+                                ref={emailRef}
+                                keyboardType="email-address"
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={() => passwordRef.current?.focus()}
+                            />
+
+                            <Input
+                                ref={passwordRef}
+                                name="password"
+                                icon="lock"
+                                placeholder="Senha"
+                                secureTextEntry
+                                textContentType="newPassword"
+                                returnKeyType="send"
+                                onSubmitEditing={() => formRef.current?.submitForm()}
+                            />
+
+                            <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
+                        </Form>
+                    </Container>
+                </ScrollView>
+                <BackToSignIn
+                    onPress={() => navigation.goBack()}
+                >
+                    <Icon name="arrow-left" size={20} color="#fff" />
+                    <BackToSignInText>Voltar para logon</BackToSignInText>
+                </BackToSignIn>
+            </KeyboardAvoidingView>
+        </>
+    );
+}
+
+export default SignUp;
